a8ef0d18590e1c3b5fdb2309fb9e874159ffd483
fellipetav <43867160+fellipetav@users.noreply.github.com>
2022-05-29 14:26:27 -0300

    update theme colors

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'The fantastic Startup names generator',
-      home: RandomWords(),
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+        ),
+      ),
+      home: const RandomWords(),
     );
   }
 }
